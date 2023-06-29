741b3b21f32d539f1545242e29afa6b041e9aa72
Dash <dkshowarth@gmail.com>
2023-06-28 19:07:31 -0700

    add objects and start of for loop for password prompts

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+function generatePassword (){
+    var passData {
+      passLength: "",
+      passUpper: "",
+      passLower: "",
+      passNum: "",
+      passSpec: "",
+    }
+
+    var passQueries {
+      passLength: "How long would you like your password to be? Please write a number between 8 and 128.",
+      passUpper: "Would you like uppercase letters? Please answer Y/N.",
+      passLower: "Would you like lowercase letters? Please answer Y/N.",
+      passNum: "Would you like numbers? Please answer Y/N.",
+      passSpec: "Would you like special characters? Please answer Y/N.",
+    }
+
+    for (var i = 0, i < passData.length, i++) {
+      function getData(){
+        
+      }
+    }
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

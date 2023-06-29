0f63139f2d6c638663c30d2fa752f3a95e380e8a
Dash <dkshowarth@gmail.com>
2023-06-28 19:33:21 -0700

    add array for for loop and outline skeleton of prompt function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,46 @@
 // Assignment code here
 
 function generatePassword (){
-    var passData {
+  //create object for password answers
+    var passData = {
       passLength: "",
       passUpper: "",
       passLower: "",
       passNum: "",
       passSpec: "",
     }
-
-    var passQueries {
-      passLength: "How long would you like your password to be? Please write a number between 8 and 128.",
+    //create array to order the questions in a for loop
+    var passOrder = ["passLength", "passUpper", "passLower", "passNum", "passSpec"]
+    
+    //create object to store prompt messages
+    var passQueries = {
+      passLength: "How long would you like your password to be? Please write an integer between 8 and 128.",
       passUpper: "Would you like uppercase letters? Please answer Y/N.",
       passLower: "Would you like lowercase letters? Please answer Y/N.",
       passNum: "Would you like numbers? Please answer Y/N.",
       passSpec: "Would you like special characters? Please answer Y/N.",
     }
 
-    for (var i = 0, i < passData.length, i++) {
+    //for loop to prompt user
+    for (var i = 0, i < passOrder.length, i++) {
+      //function for making prompts and validating data
       function getData(){
-        
+        //begin infinitely nested if statements
+        if (passOrder[i] === "passLength"){
+          var promptAnswer = prompt(passQueries[i])
+            if (promptAnswer.isInteger && ( 8 <= promptAnswer <= 128) ){
+              
+            }
+            else{
+              alert("Sorry, this prompt can only take an integer between ")
+            }
+        }
+        else {
+          var promptAnswer = prompt(passQueries[i])
+          if (promptAnswer === "Y" || "N"){
+  
+          }
+        }
       }
     }
 }
